d65decc4c2e3c274706efcd2e27cb3b97ee5cc94
Felipe <felipemonzon0710@gmail.com>
2026-04-05 10:07:30 -0300

    backend inicial con express + gitignore

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('API funcionando');
+});
+
+app.listen(3000, () => {
+  console.log('Servidor en http://localhost:3000');
+});
